87bdc1f4890d1081dc0fa9d8ce569654279a2933
Jay Rome Rana <itaalyyy@gmail.com>
2025-07-19 23:36:33 +0800

    feat: extract /api from /src

--- orbyt-backend/api/index.ts
+++ orbyt-backend/api/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
-import registerRoutes from '../routes';
+import registerRoutes from '../src/routes';
 
 // Load environment variables
 dotenv.config();
